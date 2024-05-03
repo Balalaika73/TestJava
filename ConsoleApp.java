3c52e579e9f3f09dba2bf757478f14acb8298ebc
Balalaika73 <97967443+Balalaika73@users.noreply.github.com>
2024-05-03 21:46:30 +0300

    Update ConsoleApp.java

--- ConsoleApp.java
+++ ConsoleApp.java
@@ -1,15 +1,9 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        // Создаем объект Scanner для считывания ввода пользователя
-        Scanner scanner = new Scanner(System.in);
-
-        // Печатаем приветствие и запрашиваем имя пользователя
-        System.out.println("Привет! Как тебя зовут?");
-        String name = scanner.nextLine();
+        // Задаем имя пользователя
+        String name = "Иван";
 
         // Выводим персонализированное приветствие
-        System.out.println("Приятно познакомиться, " + name + "!");
+        System.out.println("Привет, " + name + "!");
     }
 }
